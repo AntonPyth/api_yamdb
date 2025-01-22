078ead97696bdec2c0bdd37379e370cb7e4b974f
Natasha Vasileva <vasilevaqa@gmail.com>
2025-01-22 10:41:45 +0100

    Add models to reviews app (Category, Genre, Title,Genre_title) + makemigrations and migrate

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,90 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Название категории"
+    )
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        indexes = [
+            models.Index(fields=["slug"]),
+        ]
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Название жанра"
+    )
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+        indexes = [
+            models.Index(fields=["slug"]),
+        ]
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Название произведения"
+    )
+    year = models.PositiveIntegerField(
+        verbose_name="Год создания"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='categorie',
+        verbose_name="Категория"
+    )
+
+    class Meta:
+        verbose_name = "Произведение"
+        verbose_name_plural = "Произведения"
+        default_related_name = "titles"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Genre_title(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='title',
+        verbose_name="Название произведения"
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        related_name='genre',
+        verbose_name="Жанр произведения"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'genre'],
+                name='unique_title_genre',
+            ),
+        ]
+
+    def __str__(self):
+        return f"{self.title} имеет {self.genre}"
