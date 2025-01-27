d513c57b195ef166ae35d5f870f836ad050740dc
Pavel Blednykh <t4903@yandex.ru>
2025-01-27 11:56:52 +0300

    upd test check. user_reg, users

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -18,22 +18,5 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and request.user.is_admin
-        )
-
-
-class IsModerator(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and request.user.is_moderator
-        )
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
         return request.user.is_admin or request.user.is_superuser
+
