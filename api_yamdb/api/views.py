fcfc7a10be9882823f80ea565f98495edade230d
Anton <138836974+AntonPyth@users.noreply.github.com>
2025-01-28 23:53:35 +0300

    Update views.py
    ./api_yamdb/api/views.py:10:80: E501 line too long (81 > 79 characters)
    ./api_yamdb/api/views.py:115:31: E999 SyntaxError: invalid syntax
    ./api_yamdb/api/views.py:115:36: W604 backticks are deprecated, use 'repr()'
    ./api_yamdb/api/views.py:115:43: E241 multiple spaces after ','
    ./api_yamdb/api/views.py:116:28: E241 multiple spaces after ','

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,7 +7,10 @@ from .utils import send_verification_email, generate_verification_code
 from rest_framework.pagination import LimitOffsetPagination
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 from rest_framework import status, filters, viewsets, mixins
 from rest_framework.decorators import action
 from .permissions import IsAdminOrReadOnly, IsAdmin, IsAuthorOrStaffOrReadOnly
