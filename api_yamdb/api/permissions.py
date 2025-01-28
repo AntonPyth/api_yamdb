4362cf415fb36b8e6bb82a8efffbcb51803e1bfe
Anton <138836974+AntonPyth@users.noreply.github.com>
2025-01-28 23:54:23 +0300

    Update permissions.py
    ./api_yamdb/api/permissions.py:10:5: E115 expected an indented block (comment)
    ./api_yamdb/api/permissions.py:15:9: E115 expected an indented block (comment)

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,12 +7,12 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-    # Разрешаем доступ всем только для методов "чтения" (GET, HEAD, OPTIONS)
+        # Разрешаем доступ всем только для методов "чтения" (GET, HEAD, OPTIONS)
         if request.method in permissions.SAFE_METHODS:
             return True
         # Проверяем, что пользователь авторизован
         if request.user.is_authenticated:
-        # Для остальных методов (POST, PUT, DELETE) разрешаем админам
+            # Для остальных методов (POST, PUT, DELETE) разрешаем админам
             return request.user.is_admin
         return False
 
