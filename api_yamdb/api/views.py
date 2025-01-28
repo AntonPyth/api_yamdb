917d5e6814e1f232b2753ac39746e1cf7d2a5ae5
Anton <138836974+AntonPyth@users.noreply.github.com>
2025-01-29 00:06:10 +0300

    Update views.py
    ./api_yamdb/api/views.py:118:80: E501 line too long (89 > 79 characters)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -115,7 +115,7 @@ class TitleViewSet(ModelViewSet):
         if not name or not year or not genre or not category:
             raise ValidationError(
                 {
-                    "detail": "Поля name, year, genre и category являются обязательными."
+                    "detail": "Поля name, year, genre, category - обязательны!"
                 }
             )
 
