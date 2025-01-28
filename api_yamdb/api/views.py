7023ba13e12706624dbca5b936d6c13d98e856ed
Anton <138836974+AntonPyth@users.noreply.github.com>
2025-01-28 23:35:33 +0300

    Update views.py
    ./api_yamdb/api/views.py:11:1: F401 'rest_framework.permissions' imported but unused

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework import status, filters, viewsets, mixins, permissions
+from rest_framework import status, filters, viewsets, mixins
 from rest_framework.decorators import action
 from .permissions import IsAdminOrReadOnly, IsAdmin, IsAuthorOrStaffOrReadOnly
 from .serializers import (
